99eae259b3b9a7f548572e69ec7e5d66ef281b18
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-04 18:02:24 -0400

    Update password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
 const AppError = require('../utils/AppError')
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 
 const sqliteConnection = require('../database/sqlite')
 
@@ -25,7 +25,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
@@ -52,7 +52,32 @@ class UsersController {
       WHERE id = ?`,
       [user.name, user.email, new Date(), id]
     )
-    //por padrão já retorna 200
+
+    if(password && !old_password) {
+      throw new AppError("Você informar a senha antiga para definir a nova senha")
+    }
+
+    if(password && old_password) {
+      //compare serve para comparar as duas senhas, mesmo uma estando criptografada
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = ?
+      WHERE id = ?`, 
+      [user.name, user.email, user.password, new Date(), id]
+    )
+
     return response.json()
   }
 }
