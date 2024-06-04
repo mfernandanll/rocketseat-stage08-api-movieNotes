d00d0a98f2f452bcc7d68cfd12e41b2cfdebca47
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-04 18:05:41 -0400

    Adjustments at update usercontroller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -41,8 +41,8 @@ class UsersController {
       throw new AppError("Este e-mail já está em uso.")
     }
 
-    user.name = name
-    user.email = email
+    user.name = name ?? user.name
+    user.email= email ?? user.email
 
     await database.run(`
       UPDATE users SET
@@ -73,9 +73,9 @@ class UsersController {
       name = ?,
       email = ?,
       password = ?,
-      updated_at = ?
+      updated_at = DATETIME('now')
       WHERE id = ?`, 
-      [user.name, user.email, user.password, new Date(), id]
+      [user.name, user.email, user.password, id]
     )
 
     return response.json()
