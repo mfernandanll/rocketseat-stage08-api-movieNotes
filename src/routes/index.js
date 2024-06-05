420916b0fbf26bb150576bd44179624f676d4227
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-05 11:49:14 -0400

    Create movie notes and movie tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 
 const usersRoutes = require('./users.routes')
+const movieNotesRoutes = require('./movieNotes.routes')
 
 const routes = Router()
 routes.use('/users', usersRoutes)
+routes.use('/movieNotes', movieNotesRoutes)
 
 module.exports = routes
