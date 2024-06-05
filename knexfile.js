b7e8438082df50ad147bcbde4550a49884481c3d
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-05 11:29:21 -0400

    Creating migration movie_tags

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,16 @@
 const path = require("path")
+
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    //funcionalidade que será executará funções no momento em que estabelecemos conexão com o banco
+    pool: {
+      //logo após criar, quero que execute essa function em que
+      //essa function serve para que o delete em cascata 
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
     },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
