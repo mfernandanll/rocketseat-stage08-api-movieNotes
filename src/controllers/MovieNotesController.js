02afea792f08ed8e0da83b1b55f016d8218790e0
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-05 12:13:26 -0400

    Listing movie notes

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -44,6 +44,16 @@ class MovieNotesController {
 
     return response.json()
   }
+
+  async index(request, response) {
+    const { user_id } = request.query
+
+    const notes = await knex("movie_notes")
+    .where({ user_id })
+    .orderBy("title")
+
+    return response.json(notes)
+  }
 }
 
 module.exports = MovieNotesController
