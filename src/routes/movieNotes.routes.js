0aaf5de587aeff35c107289770263a3af72183b0
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-05 11:56:11 -0400

    Showing movie notes

--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -7,6 +7,7 @@ const movieNotesRoutes = Router()
 const movieNotesController = new MovieNotesController()
 
 movieNotesRoutes.post('/:user_id', movieNotesController.create)
+movieNotesRoutes.get('/:id', movieNotesController.show)
 
 
 module.exports = movieNotesRoutes
