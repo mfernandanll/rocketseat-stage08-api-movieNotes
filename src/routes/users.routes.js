7917e1937e4e63725e218c1330709ada1013725f
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-12 12:06:13 -0400

    Saving images filename in database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,18 +5,18 @@ const uploadConfig = require("../configs/upload");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const UsersController = require('../controllers/UsersController')
+const UserAvatarController = require("../controllers/UserAvatarController");
 
 const usersRoutes = Router()
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig.MULTER);
 
 usersRoutes.post('/', usersController.create) 
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-  console.log(request.file.filename)
-  response.json()
-});
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
+
 
 module.exports = usersRoutes
