81e305103a88790d863a6cbe2508c71b30a0a136
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-20 17:28:18 -0400

    Installing dependencies and settings for deploy

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors')
+require("dotenv/config");
 
 const uploadConfig = require('./configs/upload');
 
@@ -35,5 +36,5 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
